b498690e0062cf2d3f5093cc204fd41666457e13
Aditya <35701044+aditya0sehgal@users.noreply.github.com>
2020-07-20 15:08:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+    include_once("index.html");
+    // include_once("index.css");
+    // include_once("index.js");
+    // include_once("india.html");
+    // include_once("india.js");
+    // include_once("news.html");
+    // include_once("news.css");
+    // include_once("news.html");
+
+?>
